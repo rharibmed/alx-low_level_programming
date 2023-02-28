5f4472dc9c2a5626118d3f5e94cfb766762a3f7e
stephy 722 <stephyshii9@gmail.com>
2023-02-28 01:01:23 -0800

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,10 @@
 #ifndef MAIN__H
 #define MAIN_H
-/*
- * Structs,enums and unions definitions
- * Typedefs
- * Function prototypes
- */
-int_putchar(char c);
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -17,5 +16,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+
 #endif
