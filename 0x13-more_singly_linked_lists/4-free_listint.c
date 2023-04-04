302f3efed5b8f074a091bc05f871a4f9b9966b54
stephy722 <stephyshii9@gmail.com>
2023-04-04 11:57:47 -0700

    update

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,21 +1,18 @@
 #include "lists.h"
 
 /**
- * free_listint - frees list
- * @head: struct head
- *
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
  */
-
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+ listint_t *temp;
 
-
-	while (head != NULL)
-	{
-		tmp = head;
-		free(head);
-		head = tmp->next;
-	}
-
+ while (head)
+ {
+ temp = head->next;
+ free(head);
+ head = temp;
+ }
 }
+
